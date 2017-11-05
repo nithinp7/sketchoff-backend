538f4dece1b807a2632159b839ec3b2e69e7184d
nrubin29 <nrubin29@gmail.com>
2017-11-04 22:44:16 -0400

    Added POST call.

--- gameEvents.js
+++ gameEvents.js
@@ -2,25 +2,50 @@
 // creates closure over returned object so that 'socket'
 // is in scope
 
-module.exports = function(socket, game) { return {
+var request = require('request');
 
+function doRequest(sketch, cb) {
+    var options = {
+        method: 'post',
+        body: {data: sketch},
+        json: true,
+        url: 'http://ml.sketchoff.net/eval'
+    };
+
+    request.post(options, function (err, res, body) {
+        console.log('error:', err); // Print the error if one occurred
+        console.log('statusCode:', res && res.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        cb(body);
+    });
+}
+
+function check(game) {
+    if(game.scoreA !== undefined && game.scoreB !== undefined) {
+        setTimeout(function() {
+            game.emit({
+                name: 'results',
+                players: [{name: game.playerA.username, score: game.scoreA, sketch: game.sketchA},
+                    {name: game.playerB.username, score: game.scoreB, sketch: game.sketchB}]
+            })
+        }, 100);
+    }
+}
+
+module.exports = function(socket, game) { return {
 	sketchUpload: function(data) {
 		if(socket === game.playerA) {
 			game.sketchA = data.sketch;
-			game.scoreA = 0 //TODO: retrieve score from neural net backend 
+			doRequest(data.sketch, function(score) {
+			    game.scoreA = parseInt(score);
+			    check(game);
+            });
 		} else if(socket === game.playerB) {
-			game.sketchB = data.sketch;
-			game.scoreB = 0 //TODO: retrieve score from neural net backend 
-		}
-
-		if(game.scoreA !== undefined && game.scoreB !== undefined) {
-			setTimeout(function() {
-                game.emit({
-                    name: 'results',
-                    players: [{name: game.playerA.username, score: game.scoreA, sketch: game.sketchA},
-                        {name: game.playerB.username, score: game.scoreB, sketch: game.sketchB}]
-                })
-			}, 100);
+		    game.sketchB = data.sketch;
+            doRequest(data.sketch, function(score) {
+                game.scoreB = parseInt(score);
+                check(game);
+            });
 		}
 	}
 } };
